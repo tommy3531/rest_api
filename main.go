c2acf82f21cb8c715a3ad544105c422dcbebf903
tommy3531 <thomas.marler@hotmail.com>
2018-10-28 18:48:24 -0500

    Initial code commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/gorilla/mux"
+import "net/http"
+import "flag"
+import "time"
+import "log"
+// import "github.com/gorilla/context"
+// import a "github.com/bitly/go-simplejson"
+
+
+
+func main() {
+	var dir string
+
+    flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+    flag.Parse()
+    r := mux.NewRouter()
+
+    // This will serve files under http://localhost:8000/static/<filename>
+    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
+
+    srv := &http.Server{
+        Handler:      r,
+        Addr:         "127.0.0.1:8000",
+        // Good practice: enforce timeouts for servers you create!
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+    }
+
+    log.Fatal(srv.ListenAndServe())
+}
