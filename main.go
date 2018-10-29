6217d0a25de02e245cf1909b6db0b66f472bba16
tommy3531 <thomas.marler@hotmail.com>
2018-10-28 21:54:44 -0500

    Created a simple books API

--- main.go
+++ main.go
@@ -2,31 +2,87 @@ package main
 
 import "github.com/gorilla/mux"
 import "net/http"
-import "flag"
-import "time"
 import "log"
+import "encoding/json"
+import "fmt"
 // import "github.com/gorilla/context"
 // import a "github.com/bitly/go-simplejson"
 
+type Book struct {
+	ID     string `json:id`
+	Title  string `json:title`
+	Author string `json:author`
+	Year   string `json:year`
+}
 
+var books []Book
 
 func main() {
-	var dir string
+	router := mux.NewRouter()
 
-    flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
-    flag.Parse()
-    r := mux.NewRouter()
+	books = append(books,
+		Book{ID: "1", Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
+		Book{ID: "2", Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
+		Book{ID: "3", Title: "Golang routers", Author: "Mr. Router", Year: "2012"},
+		Book{ID: "4", Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
+		Book{ID: "5", Title: "Golang good parts", Author: "Mr. Good", Year: "2014"})
 
-    // This will serve files under http://localhost:8000/static/<filename>
-    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
-    srv := &http.Server{
-        Handler:      r,
-        Addr:         "127.0.0.1:8000",
-        // Good practice: enforce timeouts for servers you create!
-        WriteTimeout: 15 * time.Second,
-        ReadTimeout:  15 * time.Second,
-    }
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
 
-    log.Fatal(srv.ListenAndServe())
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Println(params)
+
+	for _, book := range books {
+		if book.ID == params["id"] {
+			json.NewEncoder(w).Encode(&book)
+		}
+	}
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+
+	books = append(books, book)
+	json.NewEncoder(w).Encode(books)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+
+	for i, item := range books {
+		if item.ID == book.ID {
+			books[i] = book
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var book Book
+
+	book.ID = params["id"]
+
+	for i, item := range books {
+		if item.ID == book.ID {
+			books = append(books[:i], books[i+1:]...)
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
 }
